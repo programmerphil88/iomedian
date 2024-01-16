7dec17a32fd3383c4452a5be06892036f10ce4ca
programmerphil88 <156034486+programmerphil88@users.noreply.github.com>
2024-01-16 17:09:26 +0800

    Update iomedian.R

--- R/iomedian.R
+++ R/iomedian.R
@@ -1,12 +1,12 @@
-#' Inferential Median Calculator for Ordinal Data
+#' Inferential Median Function for Ordinal Data
 #' 
-#' Inferentially determines the median of ordinal (e.g. Likert) data. The input data is outrightly assumed to be ordinal in nature and not normally distributed. Performs a series of one-sample Wilcoxon Rank tests to inferentially ascertain where the ordinal data tends to gravitate. iomedian stands for "inferential ordinal median".
-#' @param input A vector of ordinal data. Type: integer.
-#' @param conflvl (optional) The desired confidence level. Default is 0.95. Type: non-negative real.
+#' Inferentially determines the median of ordinal (e.g. Likert) data. The input data is outrightly assumed to be ordinal in nature and not normally distributed. Performs a series of one-sample Wilcoxon Rank tests to inferentially ascertain where the ordinal data tends to gravitate at a chosen confidence level (default is 0.95). iomedian stands for "inferential ordinal median".
+#' @param input An array of ordinal data. Type: array of integers.
+#' @param conflvl (optional) The desired confidence level. Default is 0.95. Type: positive real.
 #' @return The inferential median. Type: integer.
 #' @examples
-#' iomedian(c(2,4,5,4,2,5,1,4,5,5,4,5,2,3,4,3,5,5,3,2,5,2,3,4,5,4,5,4,5,2),0.99)
-#' iomedian(c(2,4,5,4,2,5,1,4,5,5,4,5,2,3,4,3,5,5,3,2,5,2,3,4,5,4,5,4,5,2))
+#' iomedian(c(2,5,1,3,5,5,3,2,5,1,1),0.99)
+#' iomedian(c(2,5,1,3,5,5,3,2,5,1,1))
 #' @export
 iomedian <- function(input,conflvl){
 # iomedian stands for inferential ordinal median
